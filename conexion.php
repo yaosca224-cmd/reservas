5ed978b1bfb5ffc3d17a6538fc98ef7ae05859b1
yaosca224-cmd <yaosca224@gmail.com>
2025-11-02 11:02:23 -0600

    Simplify database connection handling

--- conexion.php
+++ conexion.php
@@ -1,9 +1,6 @@
+
 <?php
 session_start();
 require_once("funciones/fxGeneral.php");
-try {
-    $conexionPDO = fxAbrirConexion();
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
-}
+$conexionPDO = fxAbrirConexion();
 ?>
